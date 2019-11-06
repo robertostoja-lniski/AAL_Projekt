1bb2316cef391f0fb660624ab2ccff9a8f04063c
robertostoja-lniski <robert.lniski@gmail.com>
2019-11-06 22:27:28 +0100

    time counting

--- main.cpp
+++ main.cpp
@@ -14,27 +14,60 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <chrono>
 
+#define TIME_RAPORT 1
+#define VERBOSE 0
+#define TIME_RAPORT_FILE "time_raport.csv"
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    std::string filename = "test_data.txt";
+    if(argc != 2) {    
+        std::cout << "run ./a.out test_file.txt \n";
+        return 1;
+    }
+
+    std::string filename = argv[1];
     DataStorage dataStorage;
 
     dataStorage.readData(filename);
+
+#if(VERBOSE)    
     dataStorage.printData();
-    
+#endif
+
     Graph graph;
     graph.generateGraph(dataStorage);
+
+#if(VERBOSE)    
     graph.print();
+#endif
 
     Solver solver;
+
+#if(TIME_RAPORT)
+    auto s = std::chrono::high_resolution_clock::now();
+#endif
+
     unsigned int maxFlow = solver.fordFulkerson(graph.getRepresentation(), graph.getSize(), graph.getLimit());
     unsigned int requiredFlow = dataStorage.getProjectsNum();
 
+#if(TIME_RAPORT)
+    auto t = std::chrono::high_resolution_clock::now();
+    double diff = std::chrono::duration<double, std::milli>(t-s).count();
+
+    std::ofstream timeRaport;
+    timeRaport.open(TIME_RAPORT_FILE, std::ios::app );
+    timeRaport << diff << "\n";
+
+#endif
+
+#if(VERBOSE)
     if(maxFlow < requiredFlow) {
         std::cout << "Cannot assign workers.\n";
     } else {
         solver.decryptResults(dataStorage);
     }
+#endif
+
 }
